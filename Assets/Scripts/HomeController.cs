0ea7f3ecdce0302f82d4954e9a099e220d6497da
kabi_rules <javifont@gmail.com>
2017-08-03 23:44:02 +0200

    Created app icon, improved home scene. Published to Google play

--- Assets/Scripts/HomeController.cs
+++ Assets/Scripts/HomeController.cs
@@ -6,13 +6,14 @@ public class HomeController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        Time.timeScale = 1;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
 
     public void OpenMainScene()
     {
